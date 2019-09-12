9c1b875c2d105c714f84f9d66c896692c23a566b
Josh Mitchell <yoshanuikabundi@gmail.com>
2019-09-12 15:37:46 +1000

    Substantial improvements to change tracking

--- GitMarkdownLiaison.py
+++ GitMarkdownLiaison.py
@@ -1,39 +1,52 @@
 import sublime
 import sublime_plugin
 from pathlib import Path
-from functools import partial
 import re
+from zlib import adler32
+
+def hash_buffer(view):
+    '''Deterministic hash of the text in the buffer underlying a view'''
+    whole_buffer_region = sublime.Region(0, view.size())
+    text = view.substr(whole_buffer_region)
+    text_bytes = bytes(text, 'UTF-8')
+    return str(adler32(text_bytes) & 0xffffffff)
+
+def get_settings(view, setting, default=None):
+    '''Get a setting from the project file if available'''
+    try:
+        project_data = view.window().project_data()
+        project_settings = project_data['settings']['git_markdown_liaison']
+    except (KeyError, AttributeError):
+        project_settings = {}
+
+    if setting in project_settings:
+        return project_settings[setting]
+    else:
+        settings = sublime.load_settings('GitMarkdownLiaison.sublime-settings')
+        return settings.get(setting, default)
+
+def gml_active_on_view(view):
+    '''True if GitMarkdownLiaison should be active on view'''
+    if not get_settings(view, "active"):
+        return False
+
+    try:
+        path = Path(view.file_name())
+    except TypeError:
+        path = Path()
+
+    extension = path.suffix
+    allowed_extensions = get_settings(view, "extensions")
+    if extension in allowed_extensions:
+        return True
+    else:
+        return False
+
 
 class GitMarkdownLiaisonCommand(sublime_plugin.TextCommand):
-    def get_settings(self, setting, default=None):
-        '''All settings can be configured in the project file'''
-        try:
-            project_data = self.view.window().project_data()
-            project_settings = project_data['settings']['git_markdown_liaison']
-        except (KeyError, AttributeError):
-            project_settings = {}
-
-        if setting in project_settings:
-            return project_settings[setting]
-        else:
-            settings = sublime.load_settings('GitMarkdownLiaison.sublime-settings')
-            return settings.get(setting, default)
-
-    def is_enabled(self):
-        if not self.get_settings("active"):
-            return False
-
-        try:
-            path = Path(self.view.file_name())
-        except TypeError:
-            path = Path()
-
-        extension = path.suffix
-        allowed_extensions = self.get_settings("extensions")
-        if extension in allowed_extensions:
-            return True
-        else:
-            return False
+    '''Base class for commands in this module'''
+    def get_settings(self, *args, **kwargs):
+        return get_settings(self.view, *args, **kwargs)
 
     def is_visible(self):
         return False
@@ -63,18 +76,102 @@ class RemoveNewlinesLiaison(GitMarkdownLiaisonCommand):
 class InsertNewlinesLiaison(GitMarkdownLiaisonCommand):
     pattern1 = re.compile(r'\.(\n+)')
     pattern2 = re.compile(r'\. ([^\n])')
+
     def run(self, edit):
         self.find_and_replace_all(edit, self.pattern1, r'.\n\1')
         self.find_and_replace_all(edit, self.pattern2, r'.\n\1')
 
 
 class GitMarkdownLiaisonListener(sublime_plugin.EventListener):
+    state_file = sublime.load_settings('GML_state.sublime-settings')
+    _unmodified_views_hashes = state_file.get('views', {})
+
+    @property
+    def unmodified_views_hashes(self):
+        '''Handle sublime's tardy loading of settings'''
+        if self._unmodified_views_hashes is None:
+            self.__class__.state_file = sublime.load_settings(
+                'GML_state.sublime-settings'
+            )
+            self.__class__._unmodified_views_hashes = self.state_file.get(
+                'views',
+                {}
+            )
+        return self._unmodified_views_hashes
+
     def on_pre_save(self, view):
-        view.run_command('insert_newlines_liaison')
+        if not gml_active_on_view(view):
+            return
+
+        cmd = get_settings(
+            view,
+            'to_disk_command',
+            default='insert_newlines_liaison'
+        )
+        view.run_command(cmd)
 
     def on_post_save(self, view):
-        view.run_command('remove_newlines_liaison')
+        if not gml_active_on_view(view):
+            return
+
+        cmd = get_settings(
+            view,
+            'from_disk_command',
+            default='remove_newlines_liaison'
+        )
+        view.run_command(cmd)
+        self.set_unmodified(view)
 
     def on_load(self, view):
-        view.run_command('remove_newlines_liaison')
+        if not gml_active_on_view(view):
+            return
 
+        cmd = get_settings(
+            view,
+            'from_disk_command',
+            default='remove_newlines_liaison'
+        )
+        view.run_command(cmd)
+        self.set_unmodified(view)
+
+    def on_modified(self, view):
+        '''Set scratch on if nothing's changed since saving
+
+        Makes the buffer return clean when it should be clean, and dirty
+        when it should be dirty, so Sublime knows when the file has been
+        changed since last save.
+
+        Note: This is hacky, and co-opts setting the scratch setting for
+        buffers. Hopefully I've included enough checks that it won't cause
+        any problems.'''
+        if not gml_active_on_view(view):
+            return
+
+        view_id = str(view.id())
+        if view_id not in self.unmodified_views_hashes:
+            return
+
+        if self.unmodified_views_hashes[view_id] == hash_buffer(view):
+            view.set_scratch(True)
+        else:
+            view.set_scratch(False)
+
+    def on_close(self, view):
+        '''Clean up the state when a view closes'''
+        if gml_active_on_view(view):
+            view_id = str(view.id())
+            del self.unmodified_views_hashes[view_id]
+            self.state_file.set('views', self.unmodified_views_hashes)
+            sublime.save_settings('GML_state.sublime-settings')
+
+    def set_unmodified(self, view):
+        '''Store the hash of the unmodified view after a save/load'''
+        view_id = str(view.id())
+        if view.is_scratch() and view_id not in self.unmodified_views_hashes:
+            return
+
+        self.unmodified_views_hashes[view_id] = hash_buffer(view)
+        self.state_file.set('views', self.unmodified_views_hashes)
+        sublime.save_settings('GML_state.sublime-settings')
+
+        view.set_scratch(True)
--- README.md
+++ README.md
@@ -17,21 +17,38 @@ GitMarkdownLiaison can be configured via the `GitMarkdownLiaison.sublime-setting
     "settings":
     {
         "git_markdown_liaison": {
+            // Filename extensions to operate on (if active)
             "extensions": [
                 ".md"
             ],
-
+            // Whether the plugin is active for this project
             "active": true,
+
+            // Commands to run on the view to transform the text before and after saving
+            "to_disk_command": "insert_newlines_liaison",
+            "from_disk_command": "remove_newlines_liaison",
+
+            // Selector in which newlines will be inserted/removed 
+            // (if active and with appropriate filename extension)
+            "sentence_newline_selector": "text.html.markdown meta.paragraph.markdown",
         },
     }
 }
 ```
 
+Note that the `settings` and `git_markdown_liaison` sections are only for when configuration is happening in a project file.
 
- - `extensions`: A list of filename extensions for which to enable GitMarkdownLiaison
  - `active`: Master control switch for GitMarkdownLiaison. If this is False, as it is by default, nothing will happen. I set it as False, and then enable it for individual projects where I want it.
- - `sentence_newline_selector`: The Sublime selector for the scopes where the newline manipulation should happen. By default, it just happens in Markdown paragraphs. Between this option and the `extensions` option, you can probably make this work for whatever text markup format you like.
+ - `extensions`: A list of filename extensions for which to enable GitMarkdownLiaison
+ - `sentence_newline_selector`: The Sublime selector for the scopes where the newline manipulation should happen. By default, it just happens in Markdown paragraphs. Note that this is referred to only in the liaison commands, and will not apply to any other commands used with the following two options.
+ - `to_disk_command` and `from_disk_command`: Commands to run on either side of saving or loading a file. `to_disk_command` is run before every save, and `from_disk_command` is run after every save and load. The plugin will take care of making sure the edits are transparent to Sublime. It will also ensure that they are only run in the appropriate files with respect to extension and activity, but **not** with respect to the selector.
+
+ Between these last four options, you can probably make this work for whatever you like.
 
 ## Known Issues
 
-This plugin modifies the buffer after every save or load, meaning that every Markdown file it touches always reads as having unsaved changes. That's pretty annoying. It's also not the fastest thing ever.
+I still haven't completely cracked maintaining the scratch setting on the buffers. If a file insists that it hasn't been changed since you saved it, no matter how much you change it, try putting:
+```python
+sublime.active_window().active_view().set_scratch(False)
+```
+into the console (Ctrl+`) and saving.
